0f29ef456596c91f2811d259df7584ab75a78000
cdcjj <chihyundorischiu@gmail.com>
2017-04-24 14:44:58 -0500

    has users model

--- app/models/user.js
+++ app/models/user.js
@@ -1,3 +1,24 @@
 var db = require('../config.js');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+
+
+var User = db.Model.extend({
+  tableName: 'users',
+  initialize: () => {
+    this.on('creating', this.hashPassword);
+  },
+  comparePassword: (attempt) => {
+    var comparer = Promise.promisify(bcrypt.(compare);
+    return comparer(attempt, this.get('password'), null);
+  },
+  hashPassword: () => {
+    var hasher = Promise.promisify(bcrypt.hash);
+    return hasher(this.get('password'), null, null).bind(this)
+      .then(function(hash) {
+        this.set('password', hash);
+      });
+  }
+});
+
+modules.exports = User;
